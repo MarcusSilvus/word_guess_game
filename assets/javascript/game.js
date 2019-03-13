9f3245bf79af306cb19aee7e8c4b81ad89acd54e
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-03-13 11:47:46 -0700

    added bootstrap to html and added css stylings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,47 @@
-//Countries array
-var asia = ["China","India","Indonesia","Pakistan","Bangladesh","Japan","Philippines","Vietnam","Turkey","Iran","Thailand","Myanmar","South Korea","Iraq","Afghanistan","Saudi Arabia","Uzbekistan","Malaysia","Nepal","Yemen","North Korea","Sri Lanka","Kazakhstan","Syria","Cambodia","Jordan","Azerbaijan","United Arab Emerates","Tajikistan","Israel","Laos","Kyrgyzstan","Lebanon","Turkmenistan","Singapore","Palestine","Oman","Kuwait","Georgia","Mongolia","Armenia","Qatar","Bahrain","East Timor","Cyprus","Bhutan","Maldives","Brunei","Taiwan"];
+// Countries array
+var asia = ["China", "India", "Indonesia", "Pakistan", "Bangladesh", "Japan", "Philippines", "Vietnam", "Turkey", "Iran", "Thailand", "Myanmar", "South Korea", "Iraq", "Afghanistan", "Saudi Arabia", "Uzbekistan", "Malaysia", "Nepal", "Yemen", "North Korea", "Sri Lanka", "Kazakhstan", "Syria", "Cambodia", "Jordan", "Azerbaijan", "United Arab Emerates", "Tajikistan", "Israel", "Laos", "Kyrgyzstan", "Lebanon", "Turkmenistan", "Singapore", "Palestine", "Oman", "Kuwait", "Georgia", "Mongolia", "Armenia", "Qatar", "Bahrain", "East Timor", "Cyprus", "Bhutan", "Maldives", "Brunei", "Taiwan"];
 
-//Random country selector
-var randomCountry = asia[Math.floor(Math.random() * asia.length)];
-console.log(randomCountry);
-
-//score keeping varibles
-var wins = 0
-var loss = 0
-var guesses = 6
-var lettersGuessed = [];
+var winCount = document.getElementById("wins");
+var lossCount = document.getElementById("losses");
+var wrongLetterBank = document.getElementById("guessed"); 
+var newWord = document.getElementById("word")
 
 
 
-document.onkeyup = function (guess) {
-    var userGuess = guess.key;
-    console.log(userGuess);
+// score keeping varibles
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 6;
+var secretWord = [];
+var underScores = [];
+var wrongGuesses = [];
+var randomCountry = '';
 
+
+// Start game when button is pressed
+function newGame() {
+    // reset varibles
+    guessesRemaining = 6;
+    wrongGuesses = [];
+    underScores = [];
+
+    // randomly select a word from the asia array
+    randomCountry = asia[Math.floor(Math.random() * asia.length)];
+    console.log(randomCountry);
+
+    // show underscores for randomly selected word
+    for (var i = 0; i < randomCountry.length; i++) {
+        if (randomCountry[i] === ' ') {
+            underScores.push(' ');
+        } else {
+            underScores.push('_');
+        }
+    }
+    
 }
+
+winCount.textContent = wins;
+lossCount.textContent = losses;
+wrongLetterBank.textContent = wrongGuesses;
+newWord.textContent = randomCountry;
+
