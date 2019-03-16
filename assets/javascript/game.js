537ac7efef4e6e7548352921936874c84474b8dc
Mark Silva <marksilva@D-10-19-202-50.dhcp4.washington.edu>
2019-03-16 13:25:22 -0700

    edits to JS logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,70 +29,102 @@ for (var i = 0; i < randomCountry.length; i++) {
     } else {
         underScores.push('_');
     }
-    console.log(underScores);
 }
 
+// Start game when button is pressed
+
+function newGame() {
+    //  reset varibles
+    guessesRemaining = 6;
+    // letter bank to clear
+    wrongGuesses = [];
+
+    //new word to randomly select
+    randomCountry = asia[Math.floor(Math.random() * asia.length)];
+    console.log(randomCountry);
+
+    //clear underscores
+    underScores = [];
+
+    // show underscores for randomly selected word
+    for (var i = 0; i < randomCountry.length; i++) {
+        if (randomCountry[i] === ' ') {
+            underScores.push(' ');
+        } else {
+            underScores.push('_');
+        }
+    }
+    screenRefresh();
+}
+
+
+screenRefresh();
+
+document.getElementById("newGameButton").addEventListener("click", newGame);
+
 document.onkeyup = function (guess) {
+
 
     //when user presses a key, that key is checked against the value of the 
     //randomCountry array
     var userGuess = guess.key;
     console.log(userGuess);
+    var correct = false;
+    var alpha = "abcdefghijklmnopqrstuvwxyz";
 
-    //loop that cycles through the array
-    for (var i = 0; i < randomCountry.length; i++) {
-        if (userGuess === randomCountry[i]) {
-            underScores[i] = userGuess;
-            console.log(underScores);
-        } 
-    }
-    //NOT WORKING - want this to log the incorrect letters in the word bank and not the correct letters
-    // if (userGuess !== randomCountry[i]) {
-        wrongGuesses.push(userGuess);
-        guessesRemaining--;
-    // }
+    //This loop compares user's guess the word
+    if (alpha.includes(userGuess)) {
+        //loop that cycles through the array
+        for (var i = 0; i < randomCountry.length; i++) {
+            if (userGuess.toUpperCase() === randomCountry[i] || randomCountry[i] == userGuess.toLowerCase()) {
+                correct = true;
+                underScores[i] = randomCountry[i];
+                console.log(underScores);
+            }
+        }
+        if (correct == false) {
+            if (wrongGuesses.includes(userGuess) == false) {
+                wrongGuesses.push(userGuess);
+                guessesRemaining--;
+            }
+        }
 
-    //logs wins
-    if (underScores.join('') === randomCountry) {
-        wins++;
-        console.log("YOU WIN");
-    }
-    //logs losses
-    if (guessesRemaining <= 0) {
-        losses++;
-        console.log("YOU LOSE");
+        //logs wins
+        if (underScores.join('') === randomCountry) {
+            wins++;
+            screenRefresh();
+            alert("YOU WIN - " + randomCountry);
+            // newGame();
+        }
+
+        //logs losses
+        if (guessesRemaining <= 0) {
+            losses++;
+            alert("YOU LOSE");
+            for (var i = 0; i < randomCountry.length; i++) {
+                underScores[i] = randomCountry[i];
+            }
+        }
+
+        guessLeft.textContent = guessesRemaining;
+        //if the value is in the array, it is revealed in the appropriate spot
+        word.textContent = underScores.join('');
+        wrongLetterBank.textContent = wrongGuesses.join('  ');
+        lossCount.textContent = losses;
+        winCount.textContent = wins;
+
+        screenRefresh();
     }
 
-    guessLeft.textContent = guessesRemaining;
-    //if the value is in the array, it is revealed in the appropriate spot
-    word.textContent = underScores.join('');
-    wrongLetterBank.textContent = wrongGuesses.join('  ');
-    lossCount.textContent = losses;
+}
+
+
+function screenRefresh() {
     winCount.textContent = wins;
-
+    lossCount.textContent = losses;
+    wrongLetterBank.textContent = wrongGuesses;
+    newWord.textContent = underScores.join('');
+    guessLeft.textContent = guessesRemaining;
+    wrongLetterBank.textContent = wrongGuesses.join('  ');
 }
-
-
-
-// Start game when button is pressed
-function newGame() {
-    // reset varibles
-    guessesRemaining = 6;
-    wrongGuesses = [];
-    secretWord = [];
-
-    // randomly select a word from the asia array
-    randomCountry = asia[Math.floor(Math.random() * asia.length)];
-    console.log(randomCountry);
-
-    // show underscores for randomly selected word
-
-
-}
-
-winCount.textContent = wins;
-lossCount.textContent = losses;
-wrongLetterBank.textContent = wrongGuesses;
-newWord.textContent = underScores.join('');
-guessLeft.textContent = guessesRemaining;
 
